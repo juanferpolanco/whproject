462e85f0791e07356d763d466817b880897c1592
Juan Polanco <juan.polanco.franco@gmail.com>
2025-05-05 16:07:49 -0500

    reservation table created

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from reservations.models import Reservation
 
 # Register your models here.
+
+admin.site.register(Reservation)
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apartments.models import Apartment
 
-# Create your models here.
+class Reservation(models.Model):
+    Apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    StartDate = models.DateField()
+    EndDate = models.DateField()
+    TotalPrice = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # def __str__(self):
+    #     return f"Reservacion hecha por {self.user.username} desde {self.start_date} hasta {self.end_date}"
